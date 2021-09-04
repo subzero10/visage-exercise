966aebfc35c3f8d76176fff3b1521383bd6b419f
Pangratios Cosma <pagcosma@gmail.com>
2021-09-04 17:01:10 +0300

    history fallback for vue-router

--- backend/src/http/app.js
+++ backend/src/http/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const config = require('../utils/config');
+const history = require('connect-history-api-fallback');
 
 //init db connection
 require('../database/models');
 
 const app = express();
 
+app.use(history());
 app.use(bodyParser.json());
 app.use(express.static(config.clientSideAppPath));
 app.use('/', routes);
